dcf914f21569f87e34e31032ccb05196d1e0a438
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2014-11-21 00:40:12 +0100

    Adding karma and jshint to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,36 @@
 module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-serve');
+	grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
 	grunt.initConfig({
 	    serve: {
 	        options: {
 	            port: 9000
 	        }
-	    }
+	    },
+	    karma: {
+	    	unit: {
+	    		configFile: 'test/karma.conf.js',
+	    		singleRun: true
+	    	}
+	    },
+	    jshint: {
+	    	options: {
+	    		curly: true,
+	    		eqeqeq: true,
+	    		eqnull: true,
+	    		expr: true
+	    	},
+	    	files: {
+	    		src: ['apps/**/!(*Spec).js']
+		  	},
+		},
 	});
+
+	grunt.registerTask('test', ['karma']);
+	grunt.registerTask('hint', ['jshint']);
+
 	grunt.registerTask('default', ['serve']);
 };
