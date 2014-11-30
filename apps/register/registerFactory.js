8903d93270ace4aec4c980ab35c5b4d5fb8ba130
Borja Fernández Pillado <borja.fpillado@gmail.com>
2014-11-30 01:57:02 +0100

    Creating register page and functionality

---
+++ apps/register/registerFactory.js
@@ -0,0 +1,30 @@
+angular.module('register').factory('registerFactory', function ($http, $window){
+
+	var service = {};
+
+	service.register = function(register_data, callback){
+
+		if (!service.validate_params(register_data)){
+			return callback(null, "Wrong fields");
+		}
+		$http.post('http://localhost:3023/players.json', register_data)
+			.success(function(data) {
+				if (data.token){
+					$window.localStorage.setItem('my-storage', data.token);
+					service.navigate("/apps/user_profile/index.html");
+				}
+				else{
+					return callback(null, data.message);
+				}
+            })
+            .error(function(error) {
+                return callback("Ups! Something goes wrong... Let me check it out, refresh and try it again");
+            });
+	};
+
+	service.validate_params = function (register_data){
+		return register_data.username !== null && register_data.password !== null;
+	};
+
+	return service;
+});
