efcf7fdd1e1d938545eed6a86325b47b1a44cadb
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2014-12-01 23:43:52 +0100

    Adding new task for concat min and clean the angular code

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-serve');
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	grunt.initConfig({
 	    serve: {
@@ -27,10 +30,31 @@ module.exports = function(grunt) {
 	    		src: ['apps/**/!(*Spec).js']
 		  	},
 		},
-	});
+		uglify: {
+			all_my_files: {
+				files: [{
+					expand: true,
+					cwd: 'build/',
+					src: '**/*.js',
+					dest: 'build/'
+				}]
+			}
+		},
+		concat: {
+			all_my_files: {
+				files: {
+					'build/login.js': ['apps/login/**/*.js', '!apps/login/**/*Spec.js'],
+					'build/register.js': ['apps/register/**/*.js', '!apps/register/**/*Spec.js'],
+				},
+			},
+		},
 
+		clean: ["build/"]
+	});
+	
 	grunt.registerTask('test', ['karma']);
 	grunt.registerTask('hint', ['jshint']);
+	grunt.registerTask('min', ['jshint', 'clean', 'concat', 'uglify']);
 
-	grunt.registerTask('default', ['serve']);
+	grunt.registerTask('default', ['min', 'serve']);
 };
