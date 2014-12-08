2ef8fb3d2caf526cc590d0b0d17fc70786257bd6
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2014-12-08 22:29:15 +0100

    Change bower dependencies to lib/

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,8 +18,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'bower_components/angular/angular.js',
-        'bower_components/angular-mocks/angular-mocks.js',
+        'lib/angular/angular.js',
+        'lib/angular-mocks/angular-mocks.js',
         'apps/**/*.js'
     ],
 
