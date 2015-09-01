dc9e46c43243e1af7d85b87593a416fac258bc5c
BorjaL <borja.fpillado@gmail.com>
2015-09-01 23:53:40 +0200

    If any response from api is not authenticated, we redirect to login page

--- src/infraestructure/sessionInjectorFactory.js
+++ src/infraestructure/sessionInjectorFactory.js
@@ -1,10 +1,18 @@
-session_module.factory('sessionInjector', ['sessionStorageFactory', function(sessionStorageFactory) {  
+session_module.factory('sessionInjector', ['sessionStorageFactory', '$window', function(sessionStorageFactory, $window) {  
     var sessionInjector = {
         request: function(config) {
             if (sessionStorageFactory.hasSessionToken()){
                config.headers.authorization = "Bearer " + sessionStorageFactory.getSessionToken();
             }
             return config;
+        },
+        responseError: function(rejection) {
+
+        	if (rejection.status === 401){
+        		sessionStorageFactory.removeSessionInfo();
+        		$window.location.href = "/login";
+        	}
+        	return response;
         }
     };
     return sessionInjector;
