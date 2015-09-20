038891a8dcca01bb6a4e88e25cfbb200d9f0f0c4
BorjaL <borja.fpillado@gmail.com>
2015-09-20 16:33:44 +0200

    Solving multiple click button

--- src/home/homeCtrl.js
+++ src/home/homeCtrl.js
@@ -2,11 +2,16 @@ home_module.controller('homeCtrl', ['$scope', 'homeFactory', function($scope, ho
 
 	$scope.new_lead = "";
 	$scope.thank_you_message = "";
+	$scope.disable_button = false;
 
 	$scope.sendNewLead = function (){
-		if ($scope.new_lead !== ""){
+		if ($scope.new_lead !== "" && $scope.new_lead !== undefined){
+
+			$scope.disable_button = true;
+			
 			homeFactory.sendNewLead($scope.new_lead)
 				.then(function(lead_info){
+					$scope.disable_button = false;
 					$scope.new_lead = "";
 					$scope.thank_you_message = "Thank you! You will have fresh news soon";
 				}, function(error){
